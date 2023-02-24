9367957c493f1672e585d8acd24d1aec18acd67d
David Jr <david.junior@unesp.br>
2023-02-23 22:51:44 -0300

    aula-3 - using mongoose to conect to the database

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-//aula 2 - trabalhando com rotas
+//aula 3 - mongoose e conexão com o bando de dados (mongodb)
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const bodyParser = require("body-parser");
+
+const url = "mongodb://localhost:27017/learning";
+
+mongoose.connect(url);
+
+mongoose.connection.on("error", (err) => {
+	console.log("Erro na conexão com o banco de dados");
+});
+
+mongoose.connection.on("disconnected", (err) => {
+	console.log("Aplicação desconectada do banco de dados");
+});
+
+mongoose.connection.on("connected", () => {
+	console.log("Aplicação conectada ao banco de dados");
+});
+
+// configurar o body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require("./Routes/index");
 const usersRoute = require("./Routes/users");
